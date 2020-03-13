bf90001a22ba56ec79fc08ff29baee1d679143c8
shetina-shesh <shetina.shesh@bk.ru>
2020-03-13 19:15:08 +0300

    initial commit. testing sql

---
+++ src/TestServer.java
@@ -0,0 +1,35 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.swing.JOptionPane;
+
+
+public class TestServer {
+
+
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");  
+		String connectionUrl = "jdbc:sqlserver://localhost\\SQLEXPRESS;database=TestBaza;integratedSecurity=true;";  
+		//Connection con = DriverManager.getConnection(connectionUrl);
+		JOptionPane.showMessageDialog(null, "Connected");
+        
+        try (Connection con = DriverManager.getConnection(connectionUrl); Statement stmt = con.createStatement();) {
+            String SQL = "SELECT * FROM RoomTable";
+            ResultSet rs = stmt.executeQuery(SQL);
+
+            // Iterate through the data in the result set and display it.
+            while (rs.next()) {
+                System.out.println(rs.getString("ID") + " " + rs.getString("RoomNumber"));
+            }
+        }
+        // Handle any errors that may have occurred.
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
+		
+	}
+
+}

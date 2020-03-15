440f92c7e6f7010474f7e25a74dc8c97a1f483e4
shetina-shesh <shetinin86@bk.ru>
2020-03-15 13:22:24 +0300

    data output directly to the interface

--- src/SqlTestSRC/AdminConsole.java
+++ src/SqlTestSRC/AdminConsole.java
@@ -1,24 +1,12 @@
 package SqlTestSRC;
 
-import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
 import javax.swing.JButton;
 
-import java.awt.Component;
-import java.awt.GridBagLayout;
-import java.awt.GridBagConstraints;
-import java.awt.Insets;
-import javax.swing.JLabel;
-import java.awt.Button;
-import javax.swing.JTextPane;
-import java.awt.Label;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
-import java.util.ArrayList;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -35,12 +23,9 @@ public class AdminConsole extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JTextField txtAddRoom;
-	private JTextArea txtMenu;
+	private static JTextArea txtMenu;
 
-	public static void main(String[] args) throws ClassNotFoundException, SQLException{
-		 
-			
-		
+	public static void main(String[] args) throws ClassNotFoundException, SQLException{	
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
@@ -49,58 +34,6 @@ public class AdminConsole extends JFrame {
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
-			}
-		});
-	}
-
-
-	public AdminConsole() {
-		setResizable(false);
-		setTitle("Admin");
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 595, 403);
-		getContentPane().setLayout(null);
-		
-		JButton btnAddRoom = new JButton("Add room");
-		btnAddRoom.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				//method
-				room = txtAddRoom.getText();
-				
-				
-				try {
-					Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-				} catch (ClassNotFoundException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				}  
-				String connectionUrl = "jdbc:sqlserver://localhost\\SQLEXPRESS;database=TestBaza;integratedSecurity=true;";
-				
-			       try (Connection con = DriverManager.getConnection(connectionUrl); Statement stmt = con.createStatement();) {
-			            String SQL = "INSERT INTO RoomTable VALUES('"+room+"')";
-			            stmt.executeUpdate(SQL);
-			        }
-			        // Handle any errors that may have occurred.
-			        catch (SQLException e) {
-			            e.printStackTrace();
-			        }
-				
-				
-			}
-		});
-		btnAddRoom.setBounds(33, 38, 116, 25);
-		getContentPane().add(btnAddRoom);
-		
-		txtAddRoom = new JTextField();
-		txtAddRoom.setBounds(33, 76, 116, 22);
-		getContentPane().add(txtAddRoom);
-		txtAddRoom.setColumns(10);
-		
-		JButton btnShowRoom = new JButton("Show room");
-		btnShowRoom.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				//method
-				txtMenu.setText("");
 				try {
 					Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
 				} catch (ClassNotFoundException e1) {
@@ -125,8 +58,65 @@ public class AdminConsole extends JFrame {
 			        }
 			}
 		});
-		btnShowRoom.setBounds(33, 111, 116, 25);
-		getContentPane().add(btnShowRoom);
+	}
+
+
+	public AdminConsole() {
+		setResizable(false);
+		setTitle("Admin");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 595, 403);
+		getContentPane().setLayout(null);
+		
+		JButton btnAddRoom = new JButton("Add room");
+		btnAddRoom.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				//method
+				txtMenu.setText("");
+				room = txtAddRoom.getText();
+				try {
+					Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+				} catch (ClassNotFoundException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}  
+				String connectionUrl = "jdbc:sqlserver://localhost\\SQLEXPRESS;database=TestBaza;integratedSecurity=true;";
+				
+			       try (Connection con = DriverManager.getConnection(connectionUrl); Statement stmt = con.createStatement();) {
+			            String SQL = "INSERT INTO RoomTable VALUES('"+room+"')";
+			            stmt.executeUpdate(SQL);
+			        }
+			        // Handle any errors that may have occurred.
+			        catch (SQLException e) {
+			            e.printStackTrace();
+			        }
+ 
+					
+				       try (Connection con = DriverManager.getConnection(connectionUrl); Statement stmt = con.createStatement();) {
+				            String SQL = "SELECT RoomNumber FROM RoomTable";
+				            ResultSet rs = stmt.executeQuery(SQL);
+
+				            // Iterate through the data in the result set and display it.
+				            while (rs.next()) {
+				            	txtMenu.append(rs.getString("RoomNumber")+"\n");
+				                System.out.println(rs.getString("RoomNumber"));
+				            }
+				        }
+				        // Handle any errors that may have occurred.
+				        catch (SQLException e) {
+				            e.printStackTrace();
+				        }
+				
+				
+			}
+		});
+		btnAddRoom.setBounds(33, 38, 116, 25);
+		getContentPane().add(btnAddRoom);
+		
+		txtAddRoom = new JTextField();
+		txtAddRoom.setBounds(33, 76, 116, 22);
+		getContentPane().add(txtAddRoom);
+		txtAddRoom.setColumns(10);
 		
 
 		
@@ -135,6 +125,7 @@ public class AdminConsole extends JFrame {
 		getContentPane().add(txtMenu);
 		
 	}
+	
 	
 
 }
